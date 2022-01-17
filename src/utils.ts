a7747918bff7cd5bed53e16d03ed7fa666c459bf
Ethan Zitting <ethan@ethanzitting.com>
2022-01-17 13:27:25 -0600

    feat: finished getCoordsFromAddress function

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import fetch from 'cross-fetch';
-import { GOOGLE_GEOCODING_SECRET_KEY } from 'config';
+import { GOOGLE_GEOCODING_SECRET_KEY } from './config';
 
 export function parseNumber(str?: string): number | undefined {
     const parsed = Number(str);
@@ -11,25 +11,21 @@ export function _throw(msg: string): never {
     throw msg;
 }
 
-export async function useThisFunctionWheneverYouFindYourselfInTheSituationWhereYouHaveAnAddressButYouWantCoordinates(address: string): Promise<object> {
-     // Prepare the address for submission to Google's API
-    address = address.replace(/ /g, "%20");
-    let requestUrl: string = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GOOGLE_MAPS_API_KEY}`
-
-    // Submit it to the API
-    const geocodingRes = await fetch(requestUrl)
-        .catch(err => {
-            console.log(err);
-        });
-
-    console.log(geocodingRes);
-
-    // Process the response
-    const coordinates: object = {
-        lat: geocodingRes.geometry.location.lat,
-        lng: geocodingRes.geometry.location.lng
-    };
-
-    // Output the resulting Coordinates
-    return coordinates;
+export async function getCoordsFromAddress(address: string): Promise<any> {
+    const urlAddress = address.replace(/ /g, "%20");
+    const requestUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${urlAddress}&key=${GOOGLE_GEOCODING_SECRET_KEY}`
+    const coords = {
+        lat: '0',
+        lng: '0',
+    }
+    try {
+        const geocodingRes = await fetch(requestUrl)
+            .then((res) => res.json());
+        const coordRes = geocodingRes.results[0].geometry.location
+        coords.lat = coordRes.lat;
+        coords.lng = coordRes.lng;
+    } catch (error) {
+        console.log(error);
+    }
+    return coords;
 }
