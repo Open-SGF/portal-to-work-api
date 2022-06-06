2585cbbd04cdb09f7fe358e6c3e70ec9f197cc8a
Ethan Zitting <ethan@ethanzitting.com>
2022-06-05 19:38:17 -0500

    wip: fix merge conflicts

--- src/routes/services/gmail-service.ts
+++ src/routes/services/gmail-service.ts
@@ -5,14 +5,10 @@ const { google } = require('googleapis');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { authenticate } = require('@google-cloud/local-auth');
 const gmail = google.gmail('v1');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const base64 = require('base-64');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-import * as fs from "fs";
 
 import xlsx from 'xlsx';
 
-export async function runSample() {
+export async function fetchExcelFileFromEmailInbox() {
     // Obtain user credentials to use for the request
     const auth = await authenticate({
         keyfilePath: path.join(__dirname, '../../../oauth2.keys.json'),
@@ -38,24 +34,19 @@ export async function runSample() {
         })
         .then(
             (response: any) => {
-                const base64Data = base64UrlDecode(response.data.data)
+                return convertToXLSXInstance(response.data.data)
 
-                const decodedData = base64.decode(base64Data);
-
-                const workbook = xlsx.read(decodedData, { type: 'string' });
-
-                const binary = xlsx.write(workbook, { type: 'binary' });
-                fs.writeFile('./src/routes/services/workbookOne.xlsx', binary, (err: any) => console.log(err));
-
-                const bruh = Buffer.from(response.data.data, 'base64url')
-                fs.writeFile('./src/routes/services/workbookTwo.xlsx', bruh, (err: any) => console.log(err));
             },
             function (err: any) {
                 console.error('Execute error', err);
             },
         );
+}
 
+function convertToXLSXInstance (responseData: string): any {
+    const base64Data = base64UrlDecode(responseData);
 
+    return xlsx.read(base64Data, { type: 'base64'})
 }
 
 function base64UrlDecode(base64UrlData: string): string {
